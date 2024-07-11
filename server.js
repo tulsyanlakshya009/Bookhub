ed3c0c1b47953d06d562680ac68a0cc72e909c28
tulsyanlakshya09 <127251539+tulsyanlakshya009@users.noreply.github.com>
2024-07-11 10:48:29 +0800

    authors backend

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 const expressLayout = require("express-ejs-layouts");
 
 const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/authors");
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
@@ -21,5 +22,6 @@ db.on("error", error => console.error(error));
 db.once("open", () => console.log("Connected to Mongoose"));
 
 app.use("/", indexRouter);
+app.use("/authors", authorRouter);
 
 app.listen(process.env.PORT|| 3001)
